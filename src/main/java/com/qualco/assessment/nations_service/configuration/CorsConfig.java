63993458d02246327cd31bb221037a2ec8d3aea2
tzortziskap <Tzortzis.kapellas@arhs-dev-hellas.com>
2025-11-15 15:02:13 +0200

    Add CORS configuration

---
+++ src/main/java/com/qualco/assessment/nations_service/configuration/CorsConfig.java
@@ -0,0 +1,29 @@
+package com.qualco.assessment.nations_service.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@Profile("dev")
+public class CorsConfig {
+
+    @Value("${cors.allowedOrigins}")
+    private String allowedOrigins;
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(allowedOrigins)
+                        .allowedMethods("GET");
+            }
+        };
+    }
+}
